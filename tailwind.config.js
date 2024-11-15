0b57199601e8c136adb9f328feec894f20f2183f
Syed Moazam Ali <107930498+MoazamAli45@users.noreply.github.com>
2024-11-15 20:15:25 +0500

    Add custom brand color and responsive container styles; update HTML structure for video background and navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: "#E7498C",
+      },
+    },
   },
   plugins: [],
 };
